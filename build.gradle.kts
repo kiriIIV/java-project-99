e32bb414e4be81af4d48eb3c340e5c9301669cab
Кирилл <kirillvolk@MacBook-Air-Kirill.local>
2025-11-26 23:31:09 +0300

    1 step

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.8"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "7.1.0.6387"
 }
 
 group = "hexlet.code"
@@ -26,4 +27,11 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "kiriIIV_java-project-99")
+		property("sonar.organization", "kiriiiv")
+	}
 }
